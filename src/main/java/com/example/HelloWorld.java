b5d681f5e3975c194ceccbf04e74dda7e1ac24b5
Laurent Pellegrino <laurent.pellegrino@gmail.com>
2020-12-18 14:25:01 +0100

    Initial import

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import com.google.common.collect.ImmutableMap;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.validation.Validated;
+import io.micronaut.views.View;
+
+@Validated
+@Controller("/")
+public class HelloWorld {
+  @Get(value = "/soy", produces = MediaType.TEXT_HTML)
+  @View("home")
+  public HttpResponse soyDemo() {
+    // return template context
+    return HttpResponse.ok(ImmutableMap.of("username", "jdoe", "loggedIn", true));
+  }
+}
